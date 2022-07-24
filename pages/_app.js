c40c7942baae7560e34dfc946333cce9b605599e
Jeff Erson <74770087+Jeff-Erson44@users.noreply.github.com>
2022-07-25 01:26:41 +0200

    _document.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,43 @@
-import '../styles/globals.css'
+import { useEffect } from 'react'
+import { useRouter } from 'next/router'
+import Layout from '../components/Layout'
+import * as gtag from '../lib/gtatg'
+import { GTM_ID } from '../lib/gtm'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+
+  const router = useRouter()
+  useEffect (() => {
+    const handleRouteChange = (url) => {
+      gtag.pageview (url)
+    }
+    router.events.on ('routeChangeComplete', handleRouteChange)
+    return () => {
+      router.events.off ('routeChangeComplete', handleRouteChange)
+    }
+  }, [router.events])
+  return (
+    <>
+    <script
+        dangerouslySetInnerHTML={{
+          __html: `     
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}   
+            gtag('js', new Date());
+            gtag('config', '${GTM_ID}', {  
+              page_path: window.location.pathname,
+            });
+          `,
+        }}
+      />
+      
+
+        <Layout>
+          <Component {...pageProps} />
+        </Layout>
+
+    </>
+  )
 }
 
 export default MyApp
